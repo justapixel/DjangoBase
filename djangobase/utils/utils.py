2b078cad5e408eb24ec1af785df1a38400d7a9dc
Rhuan Oliveira <rhuan.dev@gmail.com>
2020-05-25 22:22:05 -0300

    feat: add kwargs params and exp_time
    Add to metabase generate token funtion two new kwargs
    params with default value '{}' and exp_time with dafault value 10

--- djangobase/utils/utils.py
+++ djangobase/utils/utils.py
@@ -5,11 +5,13 @@ from environ import Env
 env = Env()
 
 
-def metabase_generate_token(resource_type, value):
+def metabase_generate_token(resource_type, value, **kwargs):
+    params = kwargs.get('params', '{}')
+    exp_time = kwargs.get('exp_time', 10)
     payload = {
         "resource": {f"{resource_type}": value},
-        "params": {},
-        "exp": round(time.time()) + (60 * 10)  # 10 minute expiration
+        "params": params,
+        "exp": round(time.time()) + (60 * exp_time)  # 10 minute expiration
     }
     token = jwt.encode(
         payload, env('METABASE_SECRET_KEY'), algorithm="HS256")
