32102c4e2f46a136079e87fee475ed92df5984db
Rhuan Oliveira <rhuan.dev@gmail.com>
2020-05-26 22:59:23 -0300

    fix: metabase generate token params default value

--- djangobase/utils/utils.py
+++ djangobase/utils/utils.py
@@ -6,14 +6,15 @@ env = Env()
 
 
 def metabase_generate_token(resource_type, value, **kwargs):
-    params = kwargs.get('params', '{}')
+    params = kwargs.get('params', {})
     exp_time = kwargs.get('exp_time', 10)
     payload = {
         "resource": {f"{resource_type}": value},
         "params": params,
         "exp": round(time.time()) + (60 * exp_time)  # 10 minute expiration
     }
+    metabase_key = env('METABASE_SECRET_KEY')
     token = jwt.encode(
-        payload, env('METABASE_SECRET_KEY'), algorithm="HS256")
+        payload, metabase_key, algorithm="HS256")
 
     return token
